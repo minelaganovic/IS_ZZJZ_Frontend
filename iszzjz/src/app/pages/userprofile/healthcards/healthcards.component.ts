62b54700a85c4788139e994e8eb9bb8b893b44a9
Minela <minelag556@gmail.com>
2023-08-16 13:35:34 +0200

    save good function

--- iszzjz/src/app/pages/userprofile/healthcards/healthcards.component.ts
+++ iszzjz/src/app/pages/userprofile/healthcards/healthcards.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgToastService } from 'ng-angular-popup';
+import { Observable } from 'rxjs';
 import { ApiService } from 'src/app/services/api.service';
 import { AuthService } from 'src/app/services/auth.service';
 import { UserstoreService } from 'src/app/services/userstore.service';
@@ -14,6 +15,9 @@ export class HealthcardsComponent implements OnInit{
   public userinfo:any=[];
 constructor(private api: ApiService, private auth: AuthService, private store:UserstoreService, private toast: NgToastService ){
 }
+hceMap:Map<number, string> = new Map()
+
+cardsInfo$!:Observable<any[]>;
 ngOnInit(): void {
   try {
 
@@ -22,7 +26,8 @@ ngOnInit(): void {
      let infU:[] =this.auth.getArrayUFromToken();
      this.userinfo= infU;
     })
-    this.api.getCardsInfo(1)
+    this.refreshHCEMap();
+    /*this.api.getCardsInfo(1)
     .subscribe(
       {
         next:(res)=>{
@@ -31,11 +36,20 @@ ngOnInit(): void {
       error:(err)=>{
         this.toast.error({detail: "Predajte zahtev!", summary:"Nemate aktiviranu zdravstvenu knjižicu!", duration:5000})
         this.cardsinfo=null
-      }})
+      }})*/
   }
   catch(e){
     console.log("ERRRRRROR", e)
   }
- 
+}
+refreshHCEMap() {
+  this.api.getCardsInfo(this.userinfo).subscribe(data => {
+    this.cardsinfo = data;
+
+    for(let i = 0; i < data.length; i++)
+    {
+      this.hceMap.set(this.userinfo[i].user_id, this.userinfo[i].lbo);
+    }
+  })
 }
 }
